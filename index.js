adf9c97f359038834549b387636ee895f3b607f2
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2021-10-02 02:50:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ done.onclick = () => {
     })
 }*/
 
+function doneTask(iTask) {
+    let getLocalStorageData = localStorage.getItem("items");
+    toDoItems = JSON.parse(getLocalStorageData);
+    const dy = document.getElementById("bi=check2-square");
+    dy.style.textDecorationLine = "line-through";
+}
+
 function deleteTask(indexTask) {
     let getLocalStorageData = localStorage.getItem("items");
     toDoItems = JSON.parse(getLocalStorageData);
